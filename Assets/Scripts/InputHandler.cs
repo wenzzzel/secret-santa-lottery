5538f014dd251db499b8b713da1e30e40ac87722
wenzzzel <wenzzzel@live.se>
2023-10-20 20:19:46 +0200

    Added background

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -94,7 +94,7 @@ public class InputHandler : MonoBehaviour
 
         StartCoroutine(GetParticipants());
 
-        // StartCoroutine(DeleteParticipantWithDelay());
+        StartCoroutine(DeleteParticipantWithDelay());
     }
 
     private IEnumerator GetParticipants()
